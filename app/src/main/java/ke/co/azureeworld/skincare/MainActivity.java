780d2cf2868c77d3d744b48ea66d719e73dd40d2
AugustineKadima <kadimasam@gmail.com>
2021-12-28 01:21:00 +0300

    Add learn activity

--- app/src/main/java/ke/co/azureeworld/skincare/MainActivity.java
+++ app/src/main/java/ke/co/azureeworld/skincare/MainActivity.java
@@ -2,13 +2,24 @@ package ke.co.azureeworld.skincare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn_get_started;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_get_started = (Button) findViewById(R.id.btn_get_started);
+        btn_get_started.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, LandingActivity2.class));
+            }
+        });
     }
 }
