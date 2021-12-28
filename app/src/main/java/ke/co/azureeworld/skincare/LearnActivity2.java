8999e77a3a07307d6365717047c68d6c8510da6f
AugustineKadima <kadimasam@gmail.com>
2021-12-29 02:05:08 +0300

    Add skin types fragment

--- app/src/main/java/ke/co/azureeworld/skincare/LearnActivity2.java
+++ app/src/main/java/ke/co/azureeworld/skincare/LearnActivity2.java
@@ -2,13 +2,26 @@ package ke.co.azureeworld.skincare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LearnActivity2 extends AppCompatActivity {
-
+    Button btn_skin_type, btn_skin_complexion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn2);
+
+        btn_skin_complexion = (Button) findViewById(R.id.btn_skin_complexion);
+        btn_skin_type = (Button) findViewById(R.id.btn_skin_type);
+
+        btn_skin_type.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LearnActivity2.this, SkinActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/ke/co/azureeworld/skincare/ProductsActivity.java
+++ app/src/main/java/ke/co/azureeworld/skincare/ProductsActivity.java
@@ -2,13 +2,26 @@ package ke.co.azureeworld.skincare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProductsActivity extends AppCompatActivity {
-
+    Button btn_skin_type, btn_skin_complexion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_products);
+
+        btn_skin_complexion = (Button) findViewById(R.id.btn_skin_complexion);
+        btn_skin_type = (Button) findViewById(R.id.btn_skin_type);
+
+        btn_skin_complexion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ProductsActivity.this, LearnActivity2.class));
+            }
+        });
     }
 }
